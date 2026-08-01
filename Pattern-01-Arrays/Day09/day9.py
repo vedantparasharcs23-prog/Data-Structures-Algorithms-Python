a4deb94ce8b6478852ef3dec4ce429a681c43779
Vedant Parashar <vedant.parashar_cs23@gla.ac.in>
2026-08-01 18:16:45 +0530

    Complete Pattern 8 (Stack Easy): LC20, LC1047, LC682, LC844, LC155, LC232

--- Pattern-01-Arrays/Day09/day9.py
+++ Pattern-01-Arrays/Day09/day9.py
@@ -11,7 +11,7 @@ print(stack)'''
 
 
 #valid_parentheses
-s = "()[]{}"
+'''s = "()[]{}"
 
 pairs = {
     ')': '(',
@@ -48,4 +48,181 @@ for ch in s:
 if valid and not stack:
     print(True)
 else:
-    print(False)
+    print(False)'''
+
+#remove all adjacent duplicate in string
+'''s = "abbaca"
+stack=[]
+for ch in s:
+    if len(stack)==0:
+        stack.append(ch)
+
+    elif stack[-1]==ch:
+        stack.pop()
+    else:
+        stack.append(ch)
+
+ans="".join(stack)
+print(ans)'''
+
+
+#baseball Game
+
+'''ops = ["5","2","C","D","+"]
+stack=[]
+
+for op in ops:
+    if op not in "CD+":
+        stack.append(int(op))
+
+    elif op=='C':
+        stack.pop()
+
+    elif op=='D':
+        stack.append(stack[-1]*2)
+    else:
+        stack.append(stack[-1]+stack[-2])
+print(sum(stack))
+print(stack)'''
+
+
+#Backspace String Compare
+
+'''s = "ab#c"
+t = "ad#c"
+
+stack1 = []
+stack2=[]
+
+for ch in s:
+    if ch != "#":
+        stack1.append(ch)
+
+    else:
+        if stack1:
+            stack1.pop()
+
+for ch in t:
+    if ch != "#":
+        stack2.append(ch)
+
+    else:
+        if stack2:
+            stack2.pop()
+
+print(stack1 == stack2)'''
+
+
+#Min Stack
+'''stack = []
+minStack = []
+
+def push(x):
+    stack.append(x)
+
+    if not minStack:
+        minStack.append(x)
+
+    else :
+        minStack.append(min(x,minStack[-1]))
+def pop():
+    stack.pop()
+    minStack.pop()
+def top():
+    return stack[-1],minStack[-1]
+
+def getMin():
+    return minStack[-1]
+
+
+push(7)
+push(3)
+push(5)
+push(2)
+push(6)
+
+print(stack)
+print(minStack)
+
+
+
+print(getMin())
+
+pop()
+
+print(stack)
+print(minStack)
+
+print(getMin())
+
+print(top())'''
+
+#problems/implement-queue-using-stack
+'''inputStack=[]
+outputStack=[]
+
+
+def push(x):
+    inputStack.append(x)'''
+
+
+'''push(1)
+push(2)
+push(3)
+
+print(inputStack)#[1, 2, 3]
+
+print(outputStack)#[]
+
+'''
+#-------------------------------------------------------------------------------------------------------
+inputStack = []
+outputStack = []
+
+
+def push(x):
+    inputStack.append(x)
+
+
+def pop():
+    if not outputStack:
+        while inputStack:
+            outputStack.append(inputStack.pop())
+
+    return outputStack.pop()
+
+
+def peek():
+    if not outputStack:
+        while inputStack:
+            outputStack.append(inputStack.pop())
+
+    return outputStack[-1]
+
+
+def empty():
+    return not inputStack and not outputStack
+
+
+# ---------------- TEST ----------------
+
+push(1)
+push(2)
+push(3)
+
+print("Input Stack :", inputStack)
+print("Output Stack:", outputStack)
+
+print("Peek :", peek())      # 1
+
+print("Pop  :", pop())       # 1
+print("Pop  :", pop())       # 2
+
+push(4)
+
+print("Peek :", peek())      # 3
+
+print("Pop  :", pop())       # 3
+print("Pop  :", pop())       # 4
+
+print("Empty :", empty())    # True
