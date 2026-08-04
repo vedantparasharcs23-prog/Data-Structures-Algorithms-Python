4da0bb5833fee0933b1f0d2b1486ae3293ad9430
Vedant Parashar <vedant.parashar_cs23@gla.ac.in>
2026-08-04 16:11:30 +0530

    REVISION

---
+++ Pattern-01-Arrays/Day10/day11ofrevision.py
@@ -0,0 +1,40 @@
+'''
+🎉 Sliding Window Pattern Complete!
+✅ Completed Questions
+Fixed Sliding Window
+✅ 643 – Maximum Average Subarray I
+✅ 485 – Max Consecutive Ones
+✅ 2461 – Maximum Sum of Distinct Subarrays With Length K
+Variable Sliding Window
+✅ 1004 – Max Consecutive Ones III
+✅ 3 – Longest Substring Without Repeating Characters
+✅ 209 – Minimum Size Subarray Sum
+✅ 904 – Fruit Into Baskets
+
+
+'''
+
+'''
+Binary Search Roadmap
+
+Hum isi order me chalenge:
+
+704 – Binary Search
+35 – Search Insert Position
+34 – First and Last Position of Element
+278 – First Bad Version (concept only)
+852 – Peak Index in a Mountain Array
+162 – Find Peak Element
+33 – Search in Rotated Sorted Array
+81 – Search in Rotated Sorted Array II
+74 – Search a 2D Matrix
+Target
+Binary Search ka template
+Lower Bound
+Upper Bound
+First/Last Occurrence
+Peak Element
+Rotated Array
+Matrix Search
+
+'''
