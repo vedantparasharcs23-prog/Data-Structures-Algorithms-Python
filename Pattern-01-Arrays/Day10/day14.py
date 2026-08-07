2f5c1607e1970100d2c5a2c66acaf105d1f4f223
Vedant Parashar <vedant.parashar_cs23@gla.ac.in>
2026-08-07 18:18:00 +0530

    REVISION

---
+++ Pattern-01-Arrays/Day10/day14.py
@@ -0,0 +1,19 @@
+'''📘 Arrays
+LC 121 – Best Time to Buy and Sell Stock
+LC 53 – Maximum Subarray
+LC 152 – Maximum Product Subarray
+LC 169 – Majority Element
+LC 136 – Single Number
+📘 Prefix Sum / HashMap
+LC 560 – Subarray Sum Equals K
+📘 Sliding Window
+LC 209 – Minimum Size Subarray Sum
+📘 Binary Search
+LC 704 – Binary Search
+LC 35 – Search Insert Position
+LC 34 – Find First and Last Position of Element
+LC 278 – First Bad Version
+LC 852 – Peak Index in a Mountain Array
+LC 162 – Find Peak Element
+LC 33 – Search in Rotated Sorted Array
+LC 81 – Search in Rotated Sorted Array II'''
