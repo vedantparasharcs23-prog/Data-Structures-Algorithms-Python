b6630a4fffcfbbd81a68432d430f450b7f7ad967
Vedant Parashar <vedant.parashar_cs23@gla.ac.in>
2026-08-09 17:52:06 +0530

    Day16: Stack basics , Easy Stack problems and Monotonic Stack introduction revision and  5 major problem like next Greater element and daiy temperature and simplify path and evaluate RPN (Reverse Polish Notation)

--- Pattern-01-Arrays/Day15/day15.py
+++ Pattern-01-Arrays/Day15/day15.py
@@ -64,7 +64,7 @@ LC 85
 '''
 
 
-#next greater element
+#next greater element in normal array
 '''nums1 = [4, 1, 2]
 nums2 = [1, 3, 4, 2]
 
@@ -95,3 +95,119 @@ for num in nums1:
 
 print(ans)
 '''
+
+#in circular array 503
+'''nums = [1,2,1]
+
+n = len(nums)
+
+stack = []
+
+ans = [-1] * n
+
+for i in range(2 * n):
+
+    current = nums[i % n]
+
+    while stack and current > nums[stack[-1]]:
+
+        idx = stack.pop()
+
+        ans[idx] = current
+
+    if i < n:
+        stack.append(i)
+
+print(ans)'''
+
+
+#daily temperature 
+
+'''temperatures = [73,74,75,71,69,72,76,73]
+
+stack = []
+
+n = len(temperatures)
+
+ans = [0] * n
+
+for i in range(n):
+
+    current = temperatures[i]
+
+    while stack and current > temperatures[stack[-1]]:
+        idx = stack.pop()
+
+        ans[idx] = i - idx
+
+    stack.append(i)
+
+print(ans)'''
+
+
+#simplify Path 71
+
+'''path = "/a/./b/../../c/"
+
+parts = path.split("/")
+
+stack = []
+
+for part in parts:
+
+    if part == "" or part == ".":
+        continue
+    elif part == "..":
+        
+        if stack:
+            stack.pop()
+    else:
+        stack.append(part)
+    "/" + "/".join(stack)
+print("/" + "/".join(stack))'''
+
+
+#evaluate RPN 150
+
+'''tokens = ["4","13","5","/","+"]
+
+stack = []
+
+for token in tokens:
+    if token not in "+-*/":
+        stack.append(int(token))
+    else:
+        right = stack.pop()
+        left = stack.pop()
+
+        if token=="+":
+            stack.append(left + right)
+
+        elif token=="*":
+                    stack.append(left * right)
+        if token=="-":
+                    stack.append(left - right)
+        if token=="/":
+                    stack.append(int(left / right))
+
+print(stack[-1])
+'''
+'''day16
+Stack Pattern ✅
+Basics
+✅ Push
+✅ Pop
+✅ Peek
+✅ Empty
+✅ Min Stack
+LeetCode
+✅ 20 — Valid Parentheses
+✅ 1047 — Remove All Adjacent Duplicates
+✅ 844 — Backspace String Compare
+✅ 682 — Baseball Game
+✅ 155 — Min Stack
+✅ 496 — Next Greater Element I
+✅ 503 — Next Greater Element II
+✅ 739 — Daily Temperatures
+✅ 71 — Simplify Path
+✅ 150 — Evaluate Reverse Polish Notation'''
