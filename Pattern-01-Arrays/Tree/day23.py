95a945f833519b792a91815f2f95273e1023f53d
Vedant Parashar <vedant.parashar_cs23@gla.ac.in>
2026-08-16 15:53:40 +0530

    Tree: Complete BST Pattern 6

---
+++ Pattern-01-Arrays/Tree/day23.py
@@ -0,0 +1,8 @@
+'''Haan 👍 Aaj Pattern 6 ke 4 questions complete kiye:
+
+✅ LC 700 — Search in BST
+✅ LC 701 — Insert into BST
+✅ LC 98 — Validate BST
+✅ LC 530 — Minimum Absolute Difference in BST'''
+
+
