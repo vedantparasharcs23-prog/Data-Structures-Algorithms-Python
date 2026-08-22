50c6a9ae39856036732186026789f4a26be6bd7c
Vedant Parashar <vedant.parashar_cs23@gla.ac.in>
2026-08-22 14:35:13 +0530

    Tree: Complete Pattern 9 Path and LCA Problems

---
+++ Pattern-01-Arrays/Tree/day29.py
@@ -0,0 +1,9 @@
+'''Pattern 9 progress
+236 — LCA Binary Tree ✅
+235 — LCA BST ✅
+105 — Construct Tree ✅
+106 — Construct Tree ✅
+112 — Path Sum ✅
+113 — Path Sum II ✅
+437 — Path Sum III ✅
+124 — Maximum Path Sum ✅'''
