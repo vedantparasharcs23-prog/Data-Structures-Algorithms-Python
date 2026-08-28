d44e69ae44ed3ad8196021e2d5780cf5de02e10f
Vedant Parashar <vedant.parashar_cs23@gla.ac.in>
2026-08-28 16:39:00 +0530

    Learn DFS concepts and solve Number of Islands

---
+++ Pattern-01-Arrays/Graph/day5.py
@@ -0,0 +1,20 @@
+'''🟡 Pattern 3 — DFS Concepts
+✅ DFS kya hota hai
+✅ Recursive DFS
+✅ Iterative DFS (Stack)
+✅ Visited Array / Set
+✅ Connected Components
+✅ Number of Islands concept
+✅ Flood Fill using DFS
+✅ DFS on Grid
+💻 LeetCode
+✅ LC 200 — Number of Islands
+Question understanding
+DFS approach
+Boundary conditions
+Marking visited ("1" → "0")
+4-direction DFS
+Grid traversal
+Dry run
+Line-by-line code
+Indentation debugging'''
